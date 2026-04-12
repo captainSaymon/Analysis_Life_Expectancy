16c853e6812d52b74d879a4f9d1dccabb050462d
capSaymon <szymonpietruszka@onet.pl>
2026-04-12 16:30:26 +0200

    usuniecie niepotrzebnych bibliotek

--- scripts/data_operation.py
+++ scripts/data_operation.py
@@ -2,10 +2,7 @@ import pandas as pd
 import numpy as np
 
 from sklearn.model_selection import train_test_split
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.metrics import accuracy_score
 from sklearn.preprocessing import StandardScaler
-from statsmodels.stats.outliers_influence import variance_inflation_factor
 from data_fetch import DataFetch
 
 
